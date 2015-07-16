f9aa73e1baece5575b81e485818743d01dc691a0
Christoph Guttandin <chrisguttandin@media-codings.com>
2015-07-16 17:17:20 +0200

    use FirefoxDeveloper instead of Aurora

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
 
         browsers: [
             'ChromeCanary',
-            'FirefoxAurora'
+            'FirefoxDeveloper'
         ],
 
         files: [
