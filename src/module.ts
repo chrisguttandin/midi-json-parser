c89d64225f2cfb28eca888cd8566a40a484f4875
chrisguttandin <chrisguttandin@media-codings.com>
2017-02-23 03:15:02 +0100

    fix typing

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
 import { load } from 'midi-json-parser-broker';
+import { IMidiFile } from 'midi-json-parser-worker';
 import { worker } from './worker/worker';
 
 const blob: Blob = new Blob([ worker ], { type: 'application/javascript' });
@@ -7,4 +8,4 @@ const url: string = URL.createObjectURL(blob);
 
 const midiJsonParser = load(url);
 
-export const parseArrayBuffer = midiJsonParser.parseArrayBuffer;
+export const parseArrayBuffer: (arrayBuffer: ArrayBuffer) => Promise<IMidiFile> = midiJsonParser.parseArrayBuffer;
