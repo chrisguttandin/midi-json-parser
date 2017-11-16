2d08d72c39639c838f9edf3f2d635e430154a3fe
Christoph Guttandin <chrisguttandin@media-codings.com>
2017-11-16 16:05:36 +0100

    specify charset for blob

--- src/module.ts
+++ src/module.ts
@@ -2,7 +2,7 @@ import { load } from 'midi-json-parser-broker';
 import { IMidiFile } from 'midi-json-parser-worker';
 import { worker } from './worker/worker';
 
-const blob: Blob = new Blob([ worker ], { type: 'application/javascript' });
+const blob: Blob = new Blob([ worker ], { type: 'application/javascript; charset=utf-8' });
 
 const url: string = URL.createObjectURL(blob);
 
