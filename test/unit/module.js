0716446752e853035cc93171581ec343440c0f9e
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-08-08 23:34:31 +0200

    test: add additional fixture

--- test/unit/module.js
+++ test/unit/module.js
@@ -46,6 +46,7 @@ describe('module', () => {
             [ 'SubTractor 2' ],
             [ 'TheEntertainer' ],
             [ 'because' ],
+            [ 'californication' ],
             [ 'minute_waltz' ],
             [ 'rachmaninov3' ],
             [ 'scale' ],
