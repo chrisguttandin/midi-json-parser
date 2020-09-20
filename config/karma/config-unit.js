eb6d589ccfe1531b69e76e96f65fa896c734c02a
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-09-20 12:57:14 +0200

    test(config): run tests on OS X 10.15 on Travis

--- config/karma/config-unit.js
+++ config/karma/config-unit.js
@@ -56,12 +56,12 @@ module.exports = (config) => {
                 ChromeSauceLabs: {
                     base: 'SauceLabs',
                     browserName: 'chrome',
-                    platform: 'OS X 10.11'
+                    platform: 'OS X 10.15'
                 },
                 FirefoxSauceLabs: {
                     base: 'SauceLabs',
                     browserName: 'firefox',
-                    platform: 'OS X 10.11'
+                    platform: 'OS X 10.15'
                 }
             },
 
