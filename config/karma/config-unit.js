eef43b5957e8283894d554643f7d8ed7bdcb5cde
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-03-11 18:55:59 +0100

    test(config): update Karma config

--- config/karma/config-unit.js
+++ config/karma/config-unit.js
@@ -33,6 +33,8 @@ module.exports = (config) => {
             'test/unit/**/*.js': 'webpack'
         },
 
+        reporters: ['dots'],
+
         webpack: {
             mode: 'development',
             module: {
